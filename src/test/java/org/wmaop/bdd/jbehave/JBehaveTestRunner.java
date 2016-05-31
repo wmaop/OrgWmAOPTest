719573fa6c787d838b6a7d40368a5e5e721b9ed7
Richard Tomlinson <richard@sysgen.co.uk>
2016-05-31 20:21:51 +0100

    Move wm-jbehave stories into rightful location

---
+++ src/test/java/org/wmaop/bdd/jbehave/JBehaveTestRunner.java
@@ -0,0 +1,10 @@
+package org.wmaop.bdd.jbehave;
+
+import org.junit.runner.RunWith;
+
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
+
+
+@RunWith(JUnitReportingRunner.class)
+public class JBehaveTestRunner extends JBehaveRunner {
+}
